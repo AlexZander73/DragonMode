9d6bb7ef779ed4b65272a05807df090ea34cb4c9
Zach <alexanderzmackay@gmail.com>
2026-07-20 13:15:29 +1000

    Rename iOS target to DragonMode

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,7 @@ const config: CapacitorConfig = {
   backgroundColor: "#08182f",
   ios: {
     contentInset: "always",
+    scheme: "DragonMode",
   },
   android: {
     backgroundColor: "#08182f",
